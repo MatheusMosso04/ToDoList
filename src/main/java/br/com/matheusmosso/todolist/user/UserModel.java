762060376e2032d15e86742a26b5441b59ee5e00
Matheus Mosso <138066056+MatheusMosso04@users.noreply.github.com>
2026-04-27 16:15:10 -0300

    Adição de Banco de dados H2 e Tabela de Usuários

--- src/main/java/br/com/matheusmosso/todolist/user/UserModel.java
+++ src/main/java/br/com/matheusmosso/todolist/user/UserModel.java
@@ -1,32 +1,27 @@
 package br.com.matheusmosso.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data //Lib do Lombok que permite e cria os gets e sets para os atributos do model
+@Entity (name = "tb_users") //Tabela de usuários
 public class UserModel {
     
+    @Id //ID Jakarta, anotation para ser chave primária
+    @GeneratedValue(generator = "UUID") // Gerador do UUID, ID automático
+    private UUID id; //UUID, ID de identificação única e é mais seguro que ID sequenciais
+
     private String username;
     private String name;
     private String password;
 
-    public void setUsername(String username) { //Set para inserir o valor
-        this.username = username;
-    }
-
-    public String getUsername() { //Get para buscar o valor
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    @CreationTimestamp 
+    private LocalDateTime createdAt; //Quando um dado for gerado de forma automática, será registrado quando o mesmo foi gerado
 }
