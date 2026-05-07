b0131c555da9f0ab3be62d8ef3de5123d6d02f73
Matheus Mosso <138066056+MatheusMosso04@users.noreply.github.com>
2026-05-06 22:56:11 -0300

    Criptografia de senha do usuário

--- src/main/java/br/com/matheusmosso/todolist/user/UserController.java
+++ src/main/java/br/com/matheusmosso/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificadores para classes
  * public
@@ -39,6 +41,11 @@ public class UserController {
             //Nesse caso, retorna uma mensagem que o mesmo já existe e seria necessário um status code de erro != 200
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Existente");
         }
+
+        var passwordHash = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());//12 é a força, necessário verificar em documentação
+        //Criação da senha criptografada, é recuperado a senha com o get e utilizado a notação toCharArray para que seja transformado em um array de caracteres
+
+        userModel.setPassword(passwordHash);//Senha é atualizada para a senha criptografada, utilizado o método set para tal
 
         var userCreated = this.userRepository.save(userModel); //Já aqui o status code é 200 pois é cadastrado o usuário 
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
